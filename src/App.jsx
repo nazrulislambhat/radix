265041bea433d72c14c1144845eed95016227b03
Nazrul Islam <nazrulislambhat@gmail.com>
2024-09-04 10:17:48 +0530

    +

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import { Flex, Text, Button } from '@radix-ui/themes';
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Flex direction="column" gap="2">
+      <Text>Hello from Radix Themes</Text>
+      <Button>Lets go</Button>
+    </Flex>
+  );
 }
-
-export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { Theme, ThemePanel } from '@radix-ui/themes';
+import '@radix-ui/themes/styles.css';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <Theme accentColor="crimson" grayColor="sand" radius="large" scaling="95%">
     <App />
-  </StrictMode>,
-)
+    <ThemePanel />
+  </Theme>
+);
